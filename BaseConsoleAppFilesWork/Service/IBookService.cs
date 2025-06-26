3d74599acaf4da687aff77842ec1e1cdafb9a22c
GhostDragon <bk.if3@ya.ru>
2025-06-26 20:12:21 +0400

    Update IBookService.cs

--- BaseConsoleAppFilesWork/Service/IBookService.cs
+++ BaseConsoleAppFilesWork/Service/IBookService.cs
@@ -5,7 +5,7 @@ namespace BaseConsoleAppFilesWork.Service;
 public interface IBookService
 {
     public Book? GetBookById(int id);
-    public List<Book> GetBooks(List<Book> books);
+    public List<Book> GetAllBooks();
     public List<Book> AddBook(Book book);
     public List<Book> UpdateBook(Book updatedBook);
     public List<Book>  DeleteBook(int id);
