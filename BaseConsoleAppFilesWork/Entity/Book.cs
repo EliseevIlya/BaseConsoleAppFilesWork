73d9f69a0f49e080e9bc4aa1e3883b87e7bc6d00
GhostDragon <bk.if3@ya.ru>
2025-06-27 09:09:09 +0400

    update entities

--- BaseConsoleAppFilesWork/Entity/Book.cs
+++ BaseConsoleAppFilesWork/Entity/Book.cs
@@ -2,7 +2,7 @@ namespace BaseConsoleAppFilesWork.Entity;
 
 public class Book
 {
-    public int Id { get; set; }
+    public int Id { get; init; }
     public string Title { get; set; } = "";
     public string Author { get; set; } = "";
     public string Description { get; set; } = "";
--- BaseConsoleAppFilesWork/Entity/User.cs
+++ BaseConsoleAppFilesWork/Entity/User.cs
@@ -2,7 +2,7 @@ namespace BaseConsoleAppFilesWork.Entity;
 
 public class User
 {
-    public int Id { get; set; }
+    public int Id { get; init; }
     public string Name { get; set; }
     public string Email { get; set; }
     public List<Book> BorrowedBooks { get; set; } = new List<Book>();
