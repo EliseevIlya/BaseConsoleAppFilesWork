830dadaf9f946284d2ac081b82f94282cd66d917
GhostDragon <bk.if3@ya.ru>
2025-06-27 09:26:32 +0400

    update UserService

--- BaseConsoleAppFilesWork/Service/IUserService.cs
+++ BaseConsoleAppFilesWork/Service/IUserService.cs
@@ -1,6 +1,12 @@
+using BaseConsoleAppFilesWork.Entity;
+
 namespace BaseConsoleAppFilesWork.Service;
 
 public interface IUserService
 {
-    
+    User? GetUserById(int id);
+    List<User> GetAllUsers();
+    User? UpdateUser(User user);
+    string IssueBookToUser(int userId, int bookId);
+    string DeleteByIdUser(int userId);
 }
--- BaseConsoleAppFilesWork/Service/impl/UserServiceImpl.cs
+++ BaseConsoleAppFilesWork/Service/impl/UserServiceImpl.cs
@@ -2,13 +2,15 @@ using BaseConsoleAppFilesWork.Entity;
 
 namespace BaseConsoleAppFilesWork.Service.impl;
 
-public class UserServiceImpl
+public class UserServiceImpl : IUserService
 {
     private readonly IRepository<User> _userRepository;
+    private readonly BookServiceImpl _bookServiceImpl;
 
-    public UserServiceImpl(IRepository<User> userRepository)
+    public UserServiceImpl(IRepository<User> userRepository, BookServiceImpl bookServiceImpl)
     {
         _userRepository = userRepository;
+        _bookServiceImpl = bookServiceImpl;
     }
 
     public User? GetUserById(int id)
@@ -16,9 +18,49 @@ public class UserServiceImpl
         return _userRepository.GetById(id);
     }
 
+    public List<User> GetAllUsers()
+    {
+        return _userRepository.GetAll();
+    }
+
     public void AddUser(User user)
     {
         _userRepository.Add(user);
     }
-    
+
+    public User? UpdateUser(User user)
+    {
+        User? userToUpdate = _userRepository.GetById(user.Id);
+        if (userToUpdate != null)
+        {
+            userToUpdate.Name = user.Name;
+            userToUpdate.Email = user.Email;
+            
+        }
+        return userToUpdate;
+
+    }
+
+    public string IssueBookToUser(int userId, int bookId)
+    {
+        User? user = _userRepository.GetById(userId);
+        if (user == null) return $"User with id: {userId} not found";
+        
+        Book? book = _bookServiceImpl.GetBookById(bookId);
+        if (book == null) return $"Book with id: {bookId} not found";
+        
+        if (!book.IsAvailable) return $"Book with id: {bookId} not available";
+        
+        _bookServiceImpl.SetBookStatus(bookId,false);
+        user.BorrowedBooks.Add(book);
+        return $"Book with id: {bookId} has been borrowed to user {user.Name}";
+    }
+
+    public string DeleteByIdUser(int userId)
+    {
+        User? user = _userRepository.GetById(userId);
+        if (user == null) return $"User with id: {userId} not found";
+        _userRepository.DeleteById(user.Id);
+        return $"User with id: {userId} has been deleted";
+    }
 }
